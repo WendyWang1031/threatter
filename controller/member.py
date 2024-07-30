20c478e91849eb20f6d8945066bb0e0da8f291e2
WendyWang1031 <w5569590@gmail.com>
2024-07-30 22:47:24 +0800

    BackEnd : add Member system

---
+++ controller/member.py
@@ -0,0 +1,110 @@
+from model.model import *
+from service.security import security_get_current_user
+from db.member import *
+
+from fastapi import *
+from fastapi import  Depends
+from fastapi.responses import JSONResponse
+import boto3
+
+
+# def upload_file_to_s3(file: UploadFile , user_id: str):
+#     s3_client = boto3.client('s3')
+#     bucket_name = "taipei-day-trip-images"
+
+#     file_key = f"avatars/{user_id}-avatar.jpg"
+#     content_type = file.content_type
+#     try:
+#         s3_client.upload_fileobj(
+#             file.file,
+#             bucket_name,
+#             file_key,
+#             ExtraArgs={
+#                 'ContentType': content_type  
+#             }
+#         )
+#         return f"https://{bucket_name}.s3.amazonaws.com/{file_key}"
+#     except Exception as e:
+#         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
+
+async def update_member_data(
+        member_data: MemberUpdateReq ,
+        current_user : dict = Depends(security_get_current_user)) -> JSONResponse :
+    try:
+        
+        if not current_user:
+            error_response = ErrorResponse(error=True, message="User not authenticated")
+            response = JSONResponse (
+                status_code=status.HTTP_403_FORBIDDEN, 
+                content=error_response.dict())
+            return response
+        
+        member_id = current_user["account_id"]
+        fields_updated = False
+
+        # 檢查是否有效的欄位更新
+        if (member_data.name and member_data.name.strip()) or \
+            (member_data.visibility and member_data.name.visibility()) or \
+            (member_data.self_intro and member_data.self_intro.strip()) or \
+            (member_data.avatar and member_data.avatar.strip()):
+                fields_updated = True
+        
+    
+
+        if not fields_updated:
+            error_response = ErrorResponse(error=True, message="At least one field must be updated")
+            response = JSONResponse (
+                status_code=status.HTTP_400_BAD_REQUEST, 
+                content=error_response.dict())
+            return response 
+
+        
+        result = db_update_member_data(member_id , member_data)
+        if result is True:
+            success_response = SuccessfulRes(success=True)
+            response = JSONResponse(
+            status_code = status.HTTP_200_OK,
+            content=success_response.dict()
+            )
+            return response
+        else:
+            error_response = ErrorResponse(error=True, message="Failed to create member data")
+            response = JSONResponse (
+                status_code=status.HTTP_400_BAD_REQUEST, 
+                content=error_response.dict())
+            return response
+
+    
+    except Exception as e :
+        error_response = ErrorResponse(error=True, message=str(e))
+        response = JSONResponse (
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, 
+            content=error_response.dict())
+        return response
+    
+    
+async def get_member_data( account_id : str ) -> JSONResponse :
+    try:
+        member_detail = db_get_member_data(account_id)
+
+        if member_detail is not None:
+            response = JSONResponse(
+            status_code = status.HTTP_200_OK,
+            content=member_detail.dict()
+            )
+            return response
+            
+        else:
+            error_response = ErrorResponse(error=True, message="No Member Data details found for user")
+            response = JSONResponse (
+                status_code=status.HTTP_404_NOT_FOUND, 
+                content=error_response.dict())
+            return response
+
+        
+    except Exception as e :
+        error_response = ErrorResponse(error=True, message=str(e))
+        response = JSONResponse (
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, 
+            content=error_response.dict())
+        return response
---
+++ db/member.py
@@ -0,0 +1,91 @@
+from model.model import *
+import pymysql.cursors
+from typing import Any
+from db.connection_pool import get_db_connection_pool
+
+
+def db_get_member_data( account_id : str ) -> MemberDetail | None:
+    connection = get_db_connection_pool()
+    cursor = connection.cursor(pymysql.cursors.DictCursor)
+    try:
+        connection.begin()
+
+        count_sql = """select Count(*) AS fans_count
+            from member_relation
+            where target_id = %s AND relation_state = 'BeingFollow'
+        """
+        cursor.execute( count_sql , (account_id ,))
+        fans_count_data = cursor.fetchone()
+        fans_counts = fans_count_data['fans_count'] if fans_count_data else 0
+        
+        sql = """select name , account_id , avatar , self_intro , visibility 
+            from member where account_id = %s
+        """
+        cursor.execute( sql , (account_id ,))
+        member_data = cursor.fetchone()
+
+        if not member_data:
+            return None
+
+        member_detail = MemberDetail(
+            name = member_data['name'] , 
+            account_id = member_data['account_id'] ,
+            avatar = member_data.get('avatar', None) ,
+            self_intro=member_data.get('self_intro', None) ,
+            fans_counts = fans_counts ,
+            visibility = member_data['visibility'] 
+        )
+        
+        connection.commit()
+        print("member_detail:",member_detail)
+        return member_detail
+    
+    except Exception as e:
+        print(f"Error getting member data details: {e}")
+        connection.rollback()
+        return None
+    finally:
+        cursor.close()
+        connection.close()
+
+def db_update_member_data(member_id : str  , member_data : MemberUpdateReq ) -> bool :
+    connection = get_db_connection_pool()
+    cursor = connection.cursor(pymysql.cursors.DictCursor)
+    
+    try:
+        connection.begin()
+
+        def validate(value):
+            if value is None:
+                return None
+            return value.strip() or None
+        
+        data_tuple = (
+            validate(member_data.name),
+            validate(member_data.visibility),
+            validate(member_data.self_intro),
+            validate(member_data.avatar),
+            member_id
+        )
+        
+        sql = """update member 
+            SET 
+                name = COALESCE(NULLIF(%s, ''), name),
+                visibility = COALESCE(NULLIF(%s, ''), visibility),
+                self_intro = COALESCE(NULLIF(%s, ''), self_intro),
+                avatar = COALESCE(NULLIF(%s, ''), avatar)
+            where account_id = %s
+        """
+        cursor.execute(sql , ( data_tuple ))
+    
+        connection.commit()
+        
+        return True
+    
+    except Exception as e:
+        print(f"Error update Member Data: {e}")
+        connection.rollback() 
+        return False
+    finally:
+        cursor.close()
+        connection.close()
