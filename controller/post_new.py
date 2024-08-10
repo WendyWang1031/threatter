20f23d86d061cedb22e6b9869c9677d823349e71
WendyWang1031 <w5569590@gmail.com>
2024-08-10 16:44:14 +0800

    BackEnd : fix Delete Post for checking exist condition

--- controller/post_new.py
+++ controller/post_new.py
@@ -5,6 +5,7 @@ from fastapi.responses import JSONResponse
 from model.model import *
 from db.post_new import *
 from db.check_relation import *
+from db.check_post import *
 from service.security import security_get_current_user
 
 
@@ -78,33 +79,44 @@ async def get_post_home(current_user: Optional[dict], page: int) -> JSONResponse
             content=error_response.dict())
         return response
     
-async def delete_post(post_id : str ,current_user : dict = Depends(security_get_current_user)) -> JSONResponse :
+async def delete_post(post_id : str , current_user : dict = Depends(security_get_current_user)) -> JSONResponse :
     try:
-        if current_user :
-            member_id = current_user["account_id"]
-            delete_post_result = db_delete_post(post_id , member_id)
-            
-            if delete_post_result:
-                success_response = SuccessfulRes(success=True)
-                response = JSONResponse(
-                status_code = status.HTTP_200_OK,
-                content=success_response.dict()
-                )
-                return response
-            else:
-                response = JSONResponse(
-                    status_code=status.HTTP_400_BAD_REQUEST,
-                    content={
-                        "error": True,
-                        "message": str(e)
-                    })
-                return response
-        else:
+        if not current_user :
             error_response = ErrorResponse(error=True, message="User not authenticated")
             response = JSONResponse (
                 status_code=status.HTTP_403_FORBIDDEN, 
                 content=error_response.dict())
             return response
+        
+        member_id = current_user["account_id"]
+
+        post_exist_result = db_check_post_exist_or_not(member_id , post_id)
+        if post_exist_result is False:
+            error_response = ErrorResponse(error=True, message="資料庫並不存在該貼文資料")
+            response = JSONResponse (
+                status_code=status.HTTP_404_NOT_FOUND, 
+                content=error_response.dict())
+            return response
+
+        delete_post_result = db_delete_post(post_id , member_id)
+            
+        if delete_post_result:
+            success_response = SuccessfulRes(success=True)
+            response = JSONResponse(
+            status_code = status.HTTP_200_OK,
+            content=success_response.dict()
+            )
+            return response
+        else:
+            response = JSONResponse(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                content={
+                    "error": True,
+                    "message": str(e)
+                })
+            return response
+        
+            
             
         
     except Exception as e :
