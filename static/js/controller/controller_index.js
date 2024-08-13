961d22513943aee4b489340ed6542dffe950300a
WendyWang1031 <w5569590@gmail.com>
2024-08-13 15:31:15 +0800

    FrondEnd : SEPARATE the POST data

--- static/js/controller/controller_index.js
+++ static/js/controller/controller_index.js
@@ -5,14 +5,13 @@ import {
   displayMemberDetail,
 } from "../view/view_index.js";
 import { PermissionAllIcon } from "../view/view_icon.js";
-import { uploadMediaFile } from "./controller_upload.js";
 import { likePost } from "./controller_like.js";
+import { validateForm } from "./controller_submit_item.js";
 
 import { displayContentElement, displayMenuBtn } from "../view/view_posts.js";
 
 import { closeCreatePost } from "../view/view_closePost.js";
 
-const postURL = "/api/post";
 const postHomeURL = "/api/post/home";
 
 let currentPage = 0;
@@ -32,124 +31,13 @@ document.addEventListener("DOMContentLoaded", async function () {
 
   likePost();
 
-  // 提交貼文按鈕
+  // 首頁：提交貼文按鈕
   const submitPostButton = document.querySelector(".submit-post-btn");
   submitPostButton.addEventListener("click", function (event) {
     event.preventDefault();
-    validateForm();
+    validateForm("post", null, null, null);
   });
 });
-
-function validateForm() {
-  const content = document.querySelector(".post-input").value;
-
-  // 初始媒體類型的值
-  let imageFile = null;
-  let videoFile = null;
-  let audioFile = null;
-
-  // 抓取所有媒體類型
-  const imageUploadInput = document.getElementById("image-upload");
-  const videoUploadInput = document.getElementById("video-upload");
-  const audioUploadInput = document.getElementById("audio-upload");
-
-  if (imageUploadInput.files.length > 0) {
-    imageFile = imageUploadInput.files[0];
-  }
-  if (videoUploadInput.files.length > 0) {
-    videoFile = videoUploadInput.files[0];
-  }
-  if (audioUploadInput.files.length > 0) {
-    audioFile = audioUploadInput.files[0];
-  }
-  console.log("videoUploadInput:", videoUploadInput);
-
-  if (content === "" && !imageFile && !videoFile && !audioFile) {
-    alert("請填寫文字欄位或上傳圖片、影片或音源");
-    return false;
-  }
-  submitPost(content, imageFile, videoFile, audioFile);
-  return true;
-}
-
-async function submitPost(content, imageFile, videoFile, audioFile) {
-  document.getElementById("loading").classList.remove("hidden");
-  let postData = {
-    post_parent_id: null,
-    content: {
-      text: content,
-      media: {
-        images: null,
-        videos: null,
-        audios: null,
-      },
-    },
-    visibility: "public",
-  };
-
-  // 圖片
-  if (imageFile) {
-    const imageUrl = await uploadMediaFile(imageFile);
-    if (imageUrl) {
-      postData.content.media.images = imageUrl;
-    }
-  }
-
-  // 影片
-  if (videoFile) {
-    const videoUrl = await uploadMediaFile(videoFile);
-    if (videoUrl) {
-      postData.content.media.videos = videoUrl;
-    }
-  }
-
-  // 音檔
-  if (audioFile) {
-    const audioUrl = await uploadMediaFile(audioFile);
-    if (audioUrl) {
-      postData.content.media.audios = audioUrl;
-    }
-  }
-
-  try {
-    await fetchUpdatePost(postData);
-  } catch (error) {
-    console.error("Error submitting post:", error);
-    alert("Failed to submit post: " + error.message);
-  } finally {
-    document.getElementById("loading").classList.add("hidden");
-  }
-}
-
-async function fetchUpdatePost(postData) {
-  document.getElementById("loading").classList.remove("hidden");
-  const token = localStorage.getItem("userToken");
-
-  try {
-    const response = await fetch(postURL, {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${token}`,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(postData),
-    });
-
-    console.log("body:", JSON.stringify(postData));
-
-    if (!response.ok) {
-      console.log("Failed to post details:", response.status);
-
-      throw new Error(`HTTP status ${response.status}`);
-    } else {
-      window.location = "/";
-    }
-  } catch (error) {
-    console.error("Error updating post data", error);
-  } finally {
-    document.getElementById("loading").classList.add("hidden");
-  }
-}
 
 async function fetchGetPost() {
   try {
---
+++ static/js/controller/controller_submit_item.js
@@ -0,0 +1,146 @@
+import { uploadMediaFile } from "./controller_upload.js";
+
+function buildUrl({ accountId, postId, commentId, type }) {
+  const baseUrl = "/api/member";
+
+  if (type === "post") {
+    return "/api/post";
+  } else if (type === "reply") {
+    if (commentId) {
+      return `${baseUrl}/${encodeURIComponent(
+        accountId
+      )}/post/${encodeURIComponent(postId)}/comment/${encodeURIComponent(
+        commentId
+      )}/reply`;
+    } else {
+      return `${baseUrl}/${encodeURIComponent(
+        accountId
+      )}/post/${encodeURIComponent(postId)}/reply`;
+    }
+  }
+  throw new Error("Invalid type provided");
+}
+
+export function validateForm(type, accountId, postId, commentId) {
+  const content = document.querySelector(".post-input").value;
+
+  // 初始媒體類型的值
+  let imageFile = null;
+  let videoFile = null;
+  let audioFile = null;
+
+  // 抓取所有媒體類型
+  const imageUploadInput = document.getElementById("image-upload");
+  const videoUploadInput = document.getElementById("video-upload");
+  const audioUploadInput = document.getElementById("audio-upload");
+
+  if (imageUploadInput.files.length > 0) {
+    imageFile = imageUploadInput.files[0];
+  }
+  if (videoUploadInput.files.length > 0) {
+    videoFile = videoUploadInput.files[0];
+  }
+  if (audioUploadInput.files.length > 0) {
+    audioFile = audioUploadInput.files[0];
+  }
+  console.log("videoUploadInput:", videoUploadInput);
+
+  if (content === "" && !imageFile && !videoFile && !audioFile) {
+    alert("請填寫文字欄位或上傳圖片、影片或音源");
+    return false;
+  }
+  const url = buildUrl({ type, accountId, postId, commentId });
+  submitData(content, imageFile, videoFile, audioFile, url, postId);
+  return true;
+}
+
+async function submitData(
+  content,
+  imageFile,
+  videoFile,
+  audioFile,
+  url,
+  postId
+) {
+  document.getElementById("loading").classList.remove("hidden");
+
+  let data = {
+    content: {
+      text: content,
+      media: {
+        images: null,
+        videos: null,
+        audios: null,
+      },
+    },
+    visibility: "public",
+  };
+
+  // 如果是新增貼文，設定 post_parent_id
+  if (postId && url === "/api/post") {
+    data.post_parent_id = postId | null;
+  }
+
+  // 圖片
+  if (imageFile) {
+    const imageUrl = await uploadMediaFile(imageFile);
+    if (imageUrl) {
+      data.content.media.images = imageUrl;
+    }
+  }
+
+  // 影片
+  if (videoFile) {
+    const videoUrl = await uploadMediaFile(videoFile);
+    if (videoUrl) {
+      data.content.media.videos = videoUrl;
+    }
+  }
+
+  // 音檔
+  if (audioFile) {
+    const audioUrl = await uploadMediaFile(audioFile);
+    if (audioUrl) {
+      data.content.media.audios = audioUrl;
+    }
+  }
+
+  try {
+    await fetchUpdateData(data, url);
+  } catch (error) {
+    console.error("Error submitting post:", error);
+    alert("Failed to submit post: " + error.message);
+  } finally {
+    document.getElementById("loading").classList.add("hidden");
+  }
+}
+
+async function fetchUpdateData(data, url) {
+  document.getElementById("loading").classList.remove("hidden");
+  const token = localStorage.getItem("userToken");
+
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    // console.log("body:", JSON.stringify(data));
+
+    if (!response.ok) {
+      console.log("Failed to post details:", response.status);
+
+      throw new Error(`HTTP status ${response.status}`);
+    } else {
+      window.location = "/";
+    }
+  } catch (error) {
+    console.error("Error updating data", error);
+  } finally {
+    document.getElementById("loading").classList.add("hidden");
+  }
+}
