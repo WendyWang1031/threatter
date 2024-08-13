ebaef354190e2833efec9b16923084232e450abb
WendyWang1031 <w5569590@gmail.com>
2024-08-13 17:12:44 +0800

    FrondEnd : ADD the comment POST api

--- static/js/single_post.js
+++ static/js/single_post.js
@@ -1,4 +1,5 @@
 import { PermissionAllIcon } from "./view/view_icon.js";
+import { displayMemberDetail } from "./view/view_index.js";
 import {
   displayContentElement,
   displayCommentElement,
@@ -6,16 +7,31 @@ import {
   displayMenuBtn,
 } from "./view/view_posts.js";
 
+import {
+  closeCreatePost,
+  previewCreatePost,
+  displayCreateComment,
+} from "./view/view_post_detail.js";
+
+import { validateForm } from "./controller/controller_submit_item.js";
+
 import { likePost, likeCommentAndReply } from "./controller/controller_like.js";
 
 document.addEventListener("DOMContentLoaded", async function () {
   PermissionAllIcon();
+  fetchGetMemberDetail();
 
   fetchGetPost();
   likePost();
   fetchGetCommentAndReply();
 
   likeCommentAndReply();
+
+  submitComment();
+
+  closeCreatePost();
+  previewCreatePost();
+  displayCreateComment();
 
   displayMenuBtn();
 });
@@ -124,4 +140,37 @@ async function fetchGetCommentAndReply() {
   } finally {
     document.getElementById("loading").classList.add("hidden");
   }
+}
+
+async function fetchGetMemberDetail() {
+  const account_id = localStorage.getItem("account_id");
+  const memberUrl = `/api/member/${encodeURIComponent(account_id)}`;
+  if (!account_id) {
+    console.log("User not logged in, using default avatar.");
+    return;
+  }
+  try {
+    const response = await fetch(memberUrl);
+    const result = await response.json();
+
+    if (result) {
+      displayMemberDetail(result);
+    } else {
+      console.error("Failed to retrieve post data.");
+    }
+  } catch (error) {
+    console.error("Error fetching post data:", error);
+  }
+}
+
+function submitComment() {
+  const submitCommentButton = document.querySelector(".submit-post-btn");
+  submitCommentButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    const currentUrl = window.location.pathname;
+    const pathSegments = currentUrl.split("/"); // 以 "/" 分割路徑成為陣列
+    const accountId = pathSegments[2]; // 第三個元素為 account_id
+    const postId = pathSegments[4]; // 第五個元素為 post_id
+    validateForm("comment", accountId, postId, null);
+  });
 }
