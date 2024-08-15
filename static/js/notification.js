0161b3270dce7a6c5108ceaaf4b6af72337c161f
WendyWang1031 <w5569590@gmail.com>
2024-08-15 23:23:29 +0800

    FrondEnd : fix Intersection observer , nextpage

--- static/js/notification.js
+++ static/js/notification.js
@@ -1,13 +1,123 @@
 import { PermissionAllIcon } from "./view/view_icon.js";
+import { displayFollowerItem } from "./view/view_notification.js";
 
-import {
-  setupTabSwitching,
-  setupIntersectionObserver,
-} from "./controller/controller_notification.js";
+// import //   setupIntersectionObserver,
+// "./controller/controller_notification.js";
 
 document.addEventListener("DOMContentLoaded", async function () {
   PermissionAllIcon();
-
   setupTabSwitching();
-  setupIntersectionObserver();
+
+  //   setupIntersectionObserver();
 });
+
+let currentPage = 0;
+let hasNextPage = true;
+let isWaitingForData = false;
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    const firstEntry = entries[0];
+
+    if (firstEntry.isIntersecting && hasNextPage && !isWaitingForData) {
+      //調用fetch函式的時候使用非同步加載
+      const targetList = document.querySelector(".notify-req-list.active");
+      if (targetList) {
+        fetchAndDisplayFollowReq(targetList);
+      }
+    }
+  },
+  { threshold: 0.5 }
+);
+
+export function setupTabSwitching() {
+  console.log("here");
+  const tabs = document.querySelectorAll(".nav-button");
+  const notifyLists = {
+    notifyReq: document.querySelector(".notify-req-list"),
+    notifyAni: document.querySelector(".notify-ani-list"),
+  };
+
+  tabs.forEach((tab) => {
+    tab.addEventListener("click", async function () {
+      // 移除所有標籤的 active
+      tabs.forEach((t) => t.classList.remove("active"));
+      // 添加 active
+      this.classList.add("active");
+
+      // 隱藏
+      Object.values(notifyLists).forEach((list) => {
+        list.style.display = "none";
+        list.classList.remove("active");
+      });
+
+      const targetListClass = this.getAttribute("data-target");
+      const targetList = document.querySelector(`.${targetListClass}`);
+      targetList.style.display = "block";
+      targetList.classList.add("active");
+
+      if (targetListClass === "notify-req-list") {
+        await fetchAndDisplayFollowReq(targetList);
+        //   } else if (targetListClass === "notify-ani-list") {
+        //     await fetchAndDisplayFollowAni(targetList);
+      }
+    });
+  });
+}
+
+async function fetchAndDisplayFollowReq(targetList) {
+  const token = localStorage.getItem("userToken");
+
+  try {
+    isWaitingForData = true;
+    // const response = await fetch(
+    //     `/api/member/${urlAccountId}/follow/fans?page=${currentPage}`,
+    //     {
+    //       headers: headers,
+    //     }
+    //   );
+    const response = await fetch(
+      `/api/follow/member/follow?page=${currentPage}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const followReqData = await response.json();
+    console.log("followReqData:", followReqData);
+
+    // targetList.innerHTML = "";
+
+    let lastItem = document.querySelector(".list-item:last-child");
+    if (followReqData.data && followReqData.data.length > 0) {
+      currentPage++;
+      hasNextPage = followReqData.next_page != null;
+
+      followReqData.data.forEach((followerReq) => {
+        const fanItem = displayFollowerItem(followerReq);
+        targetList.appendChild(fanItem);
+      });
+
+      let newItem = document.querySelector(".list-item:last-child");
+      if (lastItem) observer.unobserve(lastItem);
+      if (hasNextPage) {
+        if (newItem) observer.observe(newItem);
+      }
+    } else {
+      hasNextPage = false;
+      const noDataMessage = document.createElement("div");
+      noDataMessage.className = "no-data-message";
+      noDataMessage.textContent = "目前尚無 要求追蹤 的對象清單";
+      targetList.appendChild(noDataMessage);
+    }
+
+    targetList.style.display = "block";
+    isWaitingForData = false;
+  } catch (error) {
+    console.error("Error fetching follower Request:", error);
+    isWaitingForData = false;
+  } finally {
+  }
+}
