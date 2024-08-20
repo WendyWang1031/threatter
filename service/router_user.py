18fde85444ea55ef262fc6d3e2bb728ef6e2d072
WendyWang1031 <w5569590@gmail.com>
2024-08-20 15:27:08 +0800

    BackEnd : remove comments log

--- service/router_user.py
+++ service/router_user.py
@@ -8,49 +8,6 @@ from fastapi.responses import JSONResponse
 
 user_router = APIRouter()
 
-# # 搜尋
-# @search_router.get("/api/search",
-#         tags= ["Search"],
-#         response_model = FollowMemberListRes , 
-#         summary = "搜尋用戶帳號",
-#         responses = {
-#             200:{
-#                 "model" : FollowMemberListRes,
-#                 "description" : "成功搜尋用戶帳號"
-#             },
-#             500:{
-#                 "model" : ErrorResponse,
-#                 "description" : "伺服器內部錯誤"
-#             }
-#          })
-# async def fetch_search(
-#     search: str = Query(..., description="輸入想要搜尋的帳號"), 
-#     page: int = Query(0, description="下一頁的頁面，如果沒有更多頁為None"),
-#     current_user : dict = Depends(security_get_current_user)) -> JSONResponse :
-#     return await get_search(search , page , current_user)
-
-
-# @app.post("/api/user" , 
-#          tags= ["User"],
-#          response_model = UserPutReq ,
-#          summary = "註冊一個新會員",
-        
-#          responses = {
-#             200:{
-#                 "model" : SuccessfulResponseForRegister,
-#                 "description" : "註冊成功"
-#             },
-#             400:{
-#                 "model" : ErrorResponse,
-#                 "description" : "註冊失敗，重複的 Email 或其他原因"
-#             },
-#             500:{
-#                 "model" : ErrorResponse,
-#                 "description" : "伺服器內部錯誤"
-#             }
-#          })
-# async def fetch_post_user_signup(user_request : UserRegisterReq) -> JSONResponse :
-#     return await register_user(user_request)
 
 #會員
 @user_router.post("/api/user" , 
