1cf6ebc97ffd75f9392a878e290c62de3ad6e9b8
WendyWang1031 <w5569590@gmail.com>
2024-08-21 18:02:51 +0800

    BackEnd : separate the insert notification

--- db/like.py
+++ db/like.py
@@ -2,6 +2,7 @@ import pymysql.cursors
 from typing import Optional
 from model.model import *
 from db.connection_pool import get_db_connection_pool
+from db.notification import *
 
 
 def db_like_post(account_id : str , post_like : LikeReq , post_id : str , member_id : str) -> bool :
@@ -108,72 +109,3 @@ def db_like_comment_or_reply(account_id : str , post_id : str , comment_like : L
         cursor.close()
         connection.close()
 
-def db_update_notification(member_id: str, account_id: str, post_id: str, content_id: str, content_type: str):
-    # 如果對自己的操作，不需紀錄
-    if member_id == account_id:
-        return
-    
-    connection = get_db_connection_pool()
-    cursor = connection.cursor(pymysql.cursors.DictCursor)
-
-    try:
-        # 先檢查是否已經存在未讀的相同通知
-        check_notification_sql = """
-            SELECT id FROM notification
-            WHERE member_id = %s 
-            AND target_id = %s 
-            AND event_type = 'Like' 
-            AND JSON_UNQUOTE(JSON_EXTRACT(event_data, '$.content_id')) = %s 
-            AND is_read = FALSE
-        """
-        cursor.execute(check_notification_sql, (member_id, account_id, content_id))
-        existing_notification = cursor.fetchone()
-        
-        # 如果已經存在就不插入通知進去表
-        if existing_notification:
-            return
-        
-        # 在按讚的狀態是TRUE的條件下，更新 notification 表的資料
-            # 先取出貼文內容資料
-        
-        content_sql = """
-            SELECT text, image, video, audio
-            FROM content
-            WHERE member_id = %s AND content_id = %s
-        """
-        cursor.execute(content_sql, (account_id, post_id))
-        post_content_data = cursor.fetchone()
-
-        media_data = Media(
-            images=post_content_data.get('image'),
-            videos=post_content_data.get('video'),
-            audios=post_content_data.get('audio')
-        )
-
-        notify_data_obj = NotifyContent(
-            post_url=f"/member/{account_id}/post/{post_id}",
-            content_id=content_id,
-            text=post_content_data['text'],
-            media=media_data
-        )
-        event_data_json = notify_data_obj.model_dump_json()
-
-        # 插入通知
-        update_notification_sql = """
-            INSERT INTO notification(
-                member_id, target_id, event_type, event_data, is_read
-            )
-            VALUES(
-                %s, %s, 'Like', %s, False
-            )
-        """
-        cursor.execute(update_notification_sql, (member_id, account_id, event_data_json))
-
-        connection.commit()
-    
-    except Exception as e:
-        print(f"Error updating notification: {e}")
-        connection.rollback()
-    finally:
-        cursor.close()
-        connection.close()
