94fc81695e8eff6aa26c4d264546a9ef887c34aa
WendyWang1031 <w5569590@gmail.com>
2024-08-21 22:28:28 +0800

    BackEnd : FIX function name and current user is not optional

--- service/router_SSE.py
+++ service/router_SSE.py
@@ -45,8 +45,8 @@ async def stream_notification(current_user: dict = Depends(security_get_current_
                 "description" : "伺服器內部錯誤"
             }
          })
-async def fetch_get_follow_fans(
-    current_user :  Optional[dict] = Depends(security_get_current_user),
+async def fetch_get_notification(
+    current_user :  dict = Depends(security_get_current_user),
     page: int = Query(0, description="下一頁的頁面，如果沒有更多頁為None")
     ) -> JSONResponse :
     return await get_notification(current_user , page)
