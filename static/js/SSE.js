eda5b00e62499c7f737ac41dff3de478e57988f0
WendyWang1031 <w5569590@gmail.com>
2024-08-23 10:19:52 +0800

    FrondEnd : add check is_read state notification

--- static/js/SSE.js
+++ static/js/SSE.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", async function () {
   SSE();
+  checkNotifications();
 });
 
 async function SSE() {
@@ -29,4 +30,42 @@ async function SSE() {
     console.error("EventSource failed:", event);
     console.log("EventSource state:", eventSource.readyState);
   };
+}
+
+async function checkNotifications() {
+  const token = localStorage.getItem("userToken");
+
+  if (!token) {
+    console.error("Token not found in localStorage");
+    return;
+  }
+
+  try {
+    const response = await fetch("/api/notification?page=0", {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+
+      // 檢查是否有未讀的通知
+      const hasUnreadNotifications = data.data.some(
+        (notification) => !notification.is_read
+      );
+
+      if (hasUnreadNotifications) {
+        // 如果有，就是顯示紅點
+        const notificationDot = document.querySelector(".notification-dot");
+        notificationDot.style.display = "block";
+      }
+    } else {
+      console.error("Failed to fetch notifications");
+    }
+  } catch (error) {
+    console.error("Error:", error);
+  }
 }
