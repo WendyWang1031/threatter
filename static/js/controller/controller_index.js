e3aff0822cba028bffc9c0a90f75f60ffbd3b1be
WendyWang1031 <w5569590@gmail.com>
2024-08-27 11:44:33 +0800

    FrondEnd : add space

--- static/js/controller/controller_index.js
+++ static/js/controller/controller_index.js
@@ -56,6 +56,7 @@ async function fetchGetPost() {
     let recommendationResponse,
       recommendationResult,
       recommendationDataAvailable = false;
+
     if (token) {
       recommendationResponse = await fetch(
         `${postHomeRecommendationURL}?page=${currentPage}`,
@@ -64,6 +65,7 @@ async function fetchGetPost() {
         }
       );
       recommendationResult = await recommendationResponse.json();
+
       // 檢查推薦貼文的結果是否有誤
       if (recommendationResult.error) {
         console.log(recommendationResult.message);
