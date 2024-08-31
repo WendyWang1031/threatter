3fb0dedb18c5463b74257e860aadfa5bf2f51247
WendyWang1031 <w5569590@gmail.com>
2024-09-01 00:45:32 +0800

    BackEnd : util error response

--- util/error_response.py
+++ util/error_response.py
@@ -12,9 +12,12 @@ DB_HAVE_NO_COMMENT_DATA_ERROR = "No COMMENT data have been found"
 DB_HAVE_NO_NOTIFICATION_DATA_ERROR = "No Notification data have been found"
 
 FAILED_UPDATE_DATA_ERROR = "FAILED to update data"
+FAILED_DELETE_DATA_ERROR = "FAILED to delete data"
 FAILED_GET_DATA_ERROR = "FAILED to get data"
+FAILED_GET_COMMENT_DATA_ERROR = "FAILED to get comment data"
 FAILED_UPDATE_MEMBER_DATA_FIELD_EMPTY_ERROR = "At least one field must be updated"
-
+FAILED_UPDATE_COMMENT_DATA_FIELD_EMPTY_ERROR = "At least one field must be updated"
+FAILED_UPDATE_REPLY_DATA_FIELD_EMPTY_ERROR = "At least one field must be updated"
 
 def bad_request_error_response(message :str):
     error_response = ErrorResponse(error=True, message=message)
@@ -42,4 +45,12 @@ def interanal_server_error_response(message :str):
     response = JSONResponse (
         status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, 
         content=error_response.dict())
+    return response
+
+def successful_response():
+    success = SuccessfulRes(success=True)
+    response = JSONResponse(
+    status_code = status.HTTP_200_OK,
+    content=success.dict()
+    )
     return response
