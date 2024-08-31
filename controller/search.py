8c6856f92a7e34c2922cf89cdc80cd661cd56cd0
WendyWang1031 <w5569590@gmail.com>
2024-08-31 23:40:41 +0800

    BackEnd : search py add util error response

--- controller/search.py
+++ controller/search.py
@@ -3,45 +3,30 @@ from fastapi import *
 from fastapi.responses import JSONResponse
 from model.model import *
 from db.search import *
+from util.error_response import *
 from service.security import security_get_current_user
-
-
 
 async def get_search(search : str , 
                      page: int,
                      current_user : dict = Depends(security_get_current_user)) -> JSONResponse :
     try:
+        member_id = current_user["account_id"] if current_user else None
+        if member_id is None :
+            return forbidden_error_response(USER_NOT_AUTHENTICATED_ERROR)
 
-        if not current_user :
-            error_response = ErrorResponse(error=True, message="User not authenticated")
-            response = JSONResponse (
-                status_code=status.HTTP_403_FORBIDDEN, 
-                content=error_response.dict())
-            return response
-        
-        member_id = current_user["account_id"]
-       
         result = db_get_search(search , page , member_id)
+
+        if result is None:
+            return bad_request_error_response(FAILED_GET_DATA_ERROR)
         
-        if result:
+        else:
             response = JSONResponse(
             status_code = status.HTTP_200_OK,
             content=result.dict()
             )
             return response
-        else:
-            error_response = ErrorResponse(error=True, message="Failed to search")
-            response = JSONResponse (
-                status_code=status.HTTP_400_BAD_REQUEST, 
-                content=error_response.dict())
-            return response
 
-    
     except Exception as e :
-        error_response = ErrorResponse(error=True, message=str(e))
-        response = JSONResponse (
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, 
-            content=error_response.dict())
-        return response
+        return interanal_server_error_response(str(e))
     
 
