5d8af73d48fcf3bb5d0b6ce1d6a94f58940bbe34
WendyWang1031 <w5569590@gmail.com>
2024-08-31 23:25:47 +0800

    BackEnd : all error response for util on like controller py

--- controller/like.py
+++ controller/like.py
@@ -6,6 +6,7 @@ from db.like import *
 from db.check_relation import *
 from db.check_post import *
 from db.update_counts import *
+from util.error_response import *
 from service.security import security_get_current_user
 
 
@@ -15,55 +16,33 @@ async def post_post_like(post_like : LikeReq ,
                         current_user : dict = Depends(security_get_current_user),
                         ) -> JSONResponse :
     try:
-        if not current_user :
-            error_response = ErrorResponse(error=True, message="User not authenticated")
-            response = JSONResponse (
-                    status_code=status.HTTP_403_FORBIDDEN, 
-                    content=error_response.dict())
-            return response
-
-        member_id = current_user["account_id"] 
+        member_id = current_user["account_id"] if current_user else None
+        if member_id is None :
+            return forbidden_error_response(USER_NOT_AUTHENTICATED_ERROR)
 
         check_results = db_check_existence_and_relations(account_id, post_id, member_id)
         if not check_results.get("user_exists"):
-            error_response = ErrorResponse(error=True, message="資料庫並不存在該用戶資料")
-            response = JSONResponse (
-                status_code=status.HTTP_404_NOT_FOUND, 
-                content=error_response.dict())
-            return response
+            return data_not_found_error_response(DB_HAVE_NO_USER_DATA_ERROR)
+        
         if post_id and not check_results.get("post_exists"):
-            error_response = ErrorResponse(error=True, message="資料庫並不存在該貼文資料")
-            response = JSONResponse (
-                status_code=status.HTTP_404_NOT_FOUND, 
-                content=error_response.dict())
-            return response
+            return data_not_found_error_response(DB_HAVE_NO_POST_DATA_ERROR)
 
 
-        total_counts = await db_like_post(account_id , post_like , post_id , member_id)
+        result = await db_like_post(account_id , post_like , post_id , member_id)
         
-        if total_counts is False:
-            error_response = ErrorResponse(error=True, message="Failed to update like data")
-            response = JSONResponse (
-                status_code=status.HTTP_400_BAD_REQUEST, 
-                content=error_response.dict())
-            return response
-        
+        if result is False:
+            return bad_request_error_response(FAILED_UPDATE_DATA_ERROR)
+
         else:
-            result = db_get_like_counts(total_counts , post_id)
-            response = JSONResponse(
-                status_code = status.HTTP_200_OK,
-                content=result.dict()
-                )
-            return response
+            result = (LikeRes(total_likes = result)).dict()
+            successful_response = JSONResponse(
+            status_code = status.HTTP_200_OK,
+            content = result
+            )
+        return successful_response
 
-
-    
     except Exception as e :
-        error_response = ErrorResponse(error=True, message=str(e))
-        response = JSONResponse (
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, 
-            content=error_response.dict())
-        return response
+        return interanal_server_error_response(str(e))
     
 
 async def post_comment_or_reply_like(comment_like : LikeReq ,
@@ -73,58 +52,36 @@ async def post_comment_or_reply_like(comment_like : LikeReq ,
                                     current_user : dict = Depends(security_get_current_user),
                         ) -> JSONResponse :
     try:
-        if not current_user :
-            error_response = ErrorResponse(error=True, message="User not authenticated")
-            response = JSONResponse (
-                    status_code=status.HTTP_403_FORBIDDEN, 
-                    content=error_response.dict())
-            return response
+        member_id = current_user["account_id"] if current_user else None
+        if member_id is None :
+            return forbidden_error_response(USER_NOT_AUTHENTICATED_ERROR)
 
-        member_id = current_user["account_id"] 
 
         check_results = db_check_existence_and_relations(account_id, post_id, comment_id, member_id)
         if not check_results.get("user_exists"):
-            error_response = ErrorResponse(error=True, message="資料庫並不存在該用戶資料")
-            response = JSONResponse (
-                status_code=status.HTTP_404_NOT_FOUND, 
-                content=error_response.dict())
-            return response
+            return data_not_found_error_response(DB_HAVE_NO_USER_DATA_ERROR)
+        
         if post_id and not check_results.get("post_exists"):
-            error_response = ErrorResponse(error=True, message="資料庫並不存在該貼文資料")
-            response = JSONResponse (
-                status_code=status.HTTP_404_NOT_FOUND, 
-                content=error_response.dict())
-            return response
+            return data_not_found_error_response(DB_HAVE_NO_POST_DATA_ERROR)
+        
         if comment_id and not check_results.get("comment_exists") and not check_results.get("reply_exists"):
-            error_response = ErrorResponse(error=True, message="資料庫並不存在該貼文底下留言資料")
-            response = JSONResponse (
-                status_code=status.HTTP_404_NOT_FOUND, 
-                content=error_response.dict())
-            return response
+            return data_not_found_error_response(DB_HAVE_NO_COMMENT_DATA_ERROR)
 
         
-        total_counts =await db_like_comment_or_reply(account_id , post_id , comment_like , comment_id , member_id)
+        result = await db_like_comment_or_reply(account_id , post_id , comment_like , comment_id , member_id)
         
-        if total_counts is False:
-            error_response = ErrorResponse(error=True, message="Failed to update like data")
-            response = JSONResponse (
-                status_code=status.HTTP_400_BAD_REQUEST, 
-                content=error_response.dict())
-            return response
+        if result is False:
+            return bad_request_error_response(FAILED_UPDATE_DATA_ERROR)
         
         else:
-            result = db_get_like_counts(total_counts , post_id)
-            response = JSONResponse(
-                status_code = status.HTTP_200_OK,
-                content=result.dict()
-                )
-            return response
+            result = (LikeRes(total_likes = result)).dict()
+            successful_response = JSONResponse(
+            status_code = status.HTTP_200_OK,
+            content = result
+            )
+        return successful_response
 
     
     except Exception as e :
-        error_response = ErrorResponse(error=True, message=str(e))
-        response = JSONResponse (
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, 
-            content=error_response.dict())
-        return response
+        return interanal_server_error_response(str(e))
   
