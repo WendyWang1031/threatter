87079a5e88ba791c43e8b12d9e43be22a5ca0205
WendyWang1031 <w5569590@gmail.com>
2024-08-31 23:53:55 +0800

    BackEnd : add util and error reponse on member page

--- controller/member.py
+++ controller/member.py
@@ -1,45 +1,20 @@
-from model.model import *
-from service.security import security_get_current_user
-from db.member import *
-
 from fastapi import *
 from fastapi import  Depends
 from fastapi.responses import JSONResponse
-import boto3
 
-
-# def upload_file_to_s3(file: UploadFile , user_id: str):
-#     s3_client = boto3.client('s3')
-#     bucket_name = "taipei-day-trip-images"
-
-#     file_key = f"avatars/{user_id}-avatar.jpg"
-#     content_type = file.content_type
-#     try:
-#         s3_client.upload_fileobj(
-#             file.file,
-#             bucket_name,
-#             file_key,
-#             ExtraArgs={
-#                 'ContentType': content_type  
-#             }
-#         )
-#         return f"https://{bucket_name}.s3.amazonaws.com/{file_key}"
-#     except Exception as e:
-#         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
+from model.model import *
+from util.error_response import *
+from service.security import security_get_current_user
+from db.member import *
 
 async def update_member_data(
         member_data: MemberUpdateReq ,
         current_user : dict = Depends(security_get_current_user)) -> JSONResponse :
     try:
-        
-        if not current_user:
-            error_response = ErrorResponse(error=True, message="User not authenticated")
-            response = JSONResponse (
-                status_code=status.HTTP_403_FORBIDDEN, 
-                content=error_response.dict())
-            return response
-        
-        member_id = current_user["account_id"]
+        member_id = current_user["account_id"] if current_user else None
+        if member_id is None :
+            return forbidden_error_response(USER_NOT_AUTHENTICATED_ERROR)
+
         fields_updated = False
        
         # 檢查是否有效的欄位更新
@@ -48,66 +23,46 @@ async def update_member_data(
             (member_data.self_intro and member_data.self_intro.strip()) or \
             (member_data.avatar and member_data.avatar.strip()):
                 fields_updated = True
-        print("member_data:",member_data)
-        print("fields_updated:",fields_updated)
-    
+        # print("member_data:",member_data)
+        # print("fields_updated:",fields_updated)
 
         if not fields_updated:
-            error_response = ErrorResponse(error=True, message="At least one field must be updated")
-            response = JSONResponse (
-                status_code=status.HTTP_400_BAD_REQUEST, 
-                content=error_response.dict())
-            return response 
-
+            return bad_request_error_response(FAILED_UPDATE_MEMBER_DATA_FIELD_EMPTY_ERROR) 
         
         result = db_update_member_data(member_id , member_data)
-        if result is True:
+
+        if result is False:
+            return bad_request_error_response(FAILED_UPDATE_DATA_ERROR) 
+        
+        else:
             success_response = SuccessfulRes(success=True)
             response = JSONResponse(
             status_code = status.HTTP_200_OK,
             content=success_response.dict()
             )
             return response
-        else:
-            error_response = ErrorResponse(error=True, message="Failed to create member data")
-            response = JSONResponse (
-                status_code=status.HTTP_400_BAD_REQUEST, 
-                content=error_response.dict())
-            return response
 
-    
     except Exception as e :
-        error_response = ErrorResponse(error=True, message=str(e))
-        response = JSONResponse (
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, 
-            content=error_response.dict())
-        return response
+        return interanal_server_error_response(str(e))
     
     
 async def get_member_data(current_user: Optional[dict] , account_id : str ) -> JSONResponse :
     try:
         member_id = current_user["account_id"] if current_user else None
-        # print(f"start member: {datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')}")
+        if member_id is None :
+            return forbidden_error_response(USER_NOT_AUTHENTICATED_ERROR)
+
         member_detail = db_get_member_data(member_id , account_id)
-        # print(f"end member: {datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')}")
-        if member_detail is not None:
-            response = JSONResponse(
+        
+        if member_detail is None:
+            return bad_request_error_response(FAILED_GET_DATA_ERROR) 
+            
+        else:
+            success_response = JSONResponse(
             status_code = status.HTTP_200_OK,
             content=member_detail.dict()
             )
-            return response
-            
-        else:
-            error_response = ErrorResponse(error=True, message="No Member Data details found for user")
-            response = JSONResponse (
-                status_code=status.HTTP_404_NOT_FOUND, 
-                content=error_response.dict())
-            return response
+            return success_response
 
-        
     except Exception as e :
-        error_response = ErrorResponse(error=True, message=str(e))
-        response = JSONResponse (
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, 
-            content=error_response.dict())
-        return response
+        return interanal_server_error_response(str(e))
