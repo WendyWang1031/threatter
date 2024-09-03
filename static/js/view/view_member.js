cb70dd74814ffd3c1a5e253e230eab40a4bd6e09
WendyWang1031 <w5569590@gmail.com>
2024-09-03 18:25:59 +0800

    FrondEnd : FIX validation for html tag to convertor on member page

--- static/js/view/view_member.js
+++ static/js/view/view_member.js
@@ -1,11 +1,11 @@
 export function displayMemberDetail(data) {
   // 主會員介面的顯示
   const name = document.querySelector(".user-name");
-  name.textContent = data.name;
+  name.textContent = escapeHtml(data.name);
   const accountId = document.querySelector(".user-account-id");
-  accountId.textContent = "@ " + data.account_id;
+  accountId.textContent = "@ " + escapeHtml(data.account_id);
   const self_intro = document.querySelector(".user-self-intro");
-  self_intro.textContent = data.self_intro;
+  self_intro.textContent = escapeHtml(data.self_intro);
   const fans_counts = document.querySelector(".user-fans");
   fans_counts.textContent = data.fans_counts + " 位粉絲";
 
@@ -28,13 +28,10 @@ export function displayMemberDetail(data) {
 
   // 編輯會員介面的顯示
   const editName = document.querySelector("#username");
-  editName.placeholder = data.name;
+  editName.placeholder = escapeHtml(data.name);
   const editSelf = document.querySelector("#bio");
-  if (data.self_intro === null) {
-    editSelf.placeholder = "";
-  } else {
-    editSelf.placeholder = data.self_intro;
-  }
+  editSelf.placeholder =
+    data.self_intro === null ? "" : escapeHtml(data.self_intro);
 
   const editAvatar = document.querySelector(".edit-profile-avatar");
   console.log("editAvatar:", editAvatar);
@@ -54,6 +51,15 @@ export function displayMemberDetail(data) {
     editImg.classList.add("profile-pic", "edit-profile-pic");
     editAvatar.append(editImg);
   }
+}
+
+function escapeHtml(unsafe) {
+  return unsafe
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
 }
 
 export function closeEditMember() {
