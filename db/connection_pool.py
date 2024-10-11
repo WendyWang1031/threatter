e73b7b5a4d960a681d1cc4ff89da4090162947c3
WendyWang1031 <w5569590@gmail.com>
2024-10-11 16:29:55 +0800

    BackEnd : DB fix to instance

--- db/connection_pool.py
+++ db/connection_pool.py
@@ -4,29 +4,40 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-maxconnections = int(os.getenv('AWS_DB_maxconnections'))
-database = os.getenv('AWS_DB_database')  
-user = os.getenv('AWS_DB_USER')  
-password = os.getenv('AWS_DB_PASSWORD')
-host = os.getenv('AWS_DB_HOST', 'localhost')
-port = int(os.getenv('AWS_DB_PORT'))
 
-pool = PooledDB(
-    creator = pymysql,
-    maxconnections = maxconnections,
-    database = database,
-    user = user,
-    password = password,
-    host = host,
-    port = port
-)
+class DBManager:
+    _db_pool = None
 
-
-def get_db_connection_pool():
-    try:
-        connetion = pool.connection()
-        print("Database connect successful")
-    except Exception as err:
-        print(f"Database connection failed : {err}")
-        raise
-    return connetion
+    @classmethod
+    def init_db_pool(cls) -> None:
+        if cls._db_pool is None:
+            try:
+                cls._db_pool = PooledDB(
+                    creator=pymysql,
+                    maxconnections=int(os.getenv('AWS_DB_maxconnections')),
+                    database=os.getenv('AWS_DB_database'),
+                    user=os.getenv('AWS_DB_USER'),
+                    password=os.getenv('AWS_DB_PASSWORD'),
+                    host=os.getenv('AWS_DB_HOST', 'localhost'),
+                    port=int(os.getenv('AWS_DB_PORT'))
+                )
+                print("資料庫連線池初始化成功")
+            except Exception as err:
+                raise RuntimeError(f"初始化資料庫連線池失敗: {err}")
+    
+    @classmethod
+    def get_connection(cls):
+        if cls._db_pool is None:
+            raise RuntimeError("資料庫連線池尚未初始化")
+        try:
+            connection = cls._db_pool.connection()
+            print("資料庫連線成功")
+            return connection
+        except Exception as err:
+            raise RuntimeError(f"取得資料庫連線失敗: {err}")
+    
+    @classmethod
+    def close_db_pool(cls):
+        if cls._db_pool:
+            cls._db_pool = None
+            print("資料庫連線池已關閉")
